1177c899921d43e267c6b23a8919b22a5140ecfd
root <logicmoo@gmail.com>
2017-03-30 02:18:32 -0700

    Autocommit 2017-03-30-02:18:26

--- prolog/prologmud_sample_games/commitpush.sh
+++ prolog/prologmud_sample_games/commitpush.sh
@@ -1,5 +1,8 @@
-git --git-dir=./games/.git --work-tree=./games pull
-git pull
+( cd ../../..
+ export d=`date +%Y-%m-%d-%H:%M:%S` ; git submodule foreach 'git commit -am "Autocommit ${d}" && git push || : ' ; git commit -am "Autocommit ${d}" && git push
+)
+#git --git-dir=./games/.git --work-tree=./games pull
+#git pull
 #% hg pull
 #% hg commit -m "$*"
 #git --git-dir=games/.git --work-tree=games commit
