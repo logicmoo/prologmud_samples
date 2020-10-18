54cdfc8722c0ca4ad36595ac6390ffde1616a8ac
Douglas R. Miles <logicmoo@gmail.com>
2020-10-18 08:39:51 -0700

    tempDir

--- prolog/prologmud_sample_games/run_mud_server.pl
+++ prolog/prologmud_sample_games/run_mud_server.pl
@@ -25,7 +25,7 @@ W:\opt\logicmoo_workspace\packs_sys\logicmoo_utils\prolog;W:\opt\logicmoo_worksp
 %:- (notrace(gtrace),nodebug).
 :- set_prolog_flag(verbose_load,true).
 :- set_prolog_flag(pfc_version,2.0).
-:- set_prolog_flag(dmsg_level,never).
+:- set_prolog_flag(dmsg_level,always).
 
 attach_packs_relative(Rel):-
    once(((
@@ -47,105 +47,9 @@ load_package_dirs:-
 :- initialization(load_package_dirs, restore_state).
 :- use_module(library(logicmoo_webui)).
 
-:- if(\+ prolog_load_context(reloading,true)).
-:- use_module(library(sandbox)).
-:- use_module(library(pengines_sandbox)).
-% :- rtrace.
 :- dmsg("Ensure run_mud_server").
 %:- rtrace(dmsg2("Ensure Run_MUD_SERVER1")).
 %:- break.
-
-:- system:use_module(library(console_input)).
-:- system:use_module(library(date)).
-:- system:use_module(library(make)).
-:- system:use_module(library(qsave)).
-:- system:use_module(library(prolog_autoload)).
-:- system:use_module(library(lists)).
-:- system:use_module(library(backcomp)).
-:- system:use_module(library(edit)).
-:- system:use_module(library(prolog_trace)).
-:- system:use_module(library(threadutil)).
-:- system:use_module(library(yall)).
-:- system:use_module(library(time)).
-:- abolish(system:time/1).
-:- system:use_module(library(statistics)).
-
-
-:- system:use_module(library(apply)).
-:- system:use_module(library(assoc)).
-:- system:use_module(library(base64)).
-:- system:use_module(library(charsio)).
-:- system:use_module(library(codesio)).
-:- system:use_module(library(crypt)).
-:- system:use_module(library(ctypes)).
-:- system:use_module(library(dialect)).
-:- system:use_module(library(doc_files)).
-:- system:use_module(library(doc_http)).
-:- system:use_module(library(edinburgh)).
-:- system:use_module(library(error)).
-:- system:use_module(library(filesex)).
-:- system:use_module(library(gensym)).
-:- system:use_module(library(git)).
-:- system:use_module(library(http/html_head)).
-:- system:use_module(library(http/http_dispatch)).
-:- system:use_module(library(http/http_path)).
-:- system:use_module(library(http/mimetype)).
-:- system:use_module(library(lazy_lists)).
-:- system:use_module(library(listing)).
-:- system:use_module(library(lists)).
-:- system:use_module(library(memfile)).
-:- system:use_module(library(modules)).
-:- system:use_module(library(nb_rbtrees)).
-:- system:use_module(library(occurs)).
-:- system:use_module(library(operators)).
-:- system:use_module(library(option)).
-:- system:use_module(library(ordsets)).
-:- system:use_module(library(pairs)).
-:- system:use_module(library(pldoc)).
-:- system:use_module(library(pldoc/doc_html)).
-:- system:use_module(library(pldoc/doc_process)).
-:- system:use_module(library(pldoc/doc_search)).
-:- system:use_module(library(pldoc/doc_util)).
-:- system:use_module(library(pldoc/man_index)).
-:- system:use_module(library(porter_stem)).
-%:- system:use_module(library(pprint)).
-:- system:use_module(library(predicate_options)).
-:- system:use_module(library(process)).
-:- system:use_module(library(prolog_clause)).
-:- system:use_module(library(prolog_code)).
-:- system:use_module(library(prolog_codewalk)).
-:- system:use_module(library(prolog_config)).
-:- system:use_module(library(prolog_source)).
-:- system:use_module(library(prolog_stack)).
-:- system:use_module(library(prolog_xref)).
-:- system:use_module(library(pure_input)).
-:- system:use_module(library(quintus)).
-:- system:use_module(library(readutil)).
-:- system:use_module(library(sgml)).
-:- system:use_module(library(sgml_write)).
-:- system:use_module(library(sha)).
-:- system:use_module(library(shell)).
-:- system:use_module(library(shlib)).
-:- system:use_module(library(socket)).
-:- system:use_module(library(solution_sequences)).
-:- system:use_module(library(sort)).
-:- system:use_module(library(ssl)).
-:- system:use_module(library(system)).
-:- system:use_module(library(thread_pool)).
-:- system:use_module(library(uri)).
-:- system:use_module(library(url)).
-:- system:use_module(library(uuid)).
-:- system:use_module(library(varnumbers)).
-:- system:use_module(library(when)).
-:- system:use_module(library(writef)).
-:- system:use_module(library(zlib)).
-
-%:- system:use_module(library(jpl)).
-%:- use_module(library(wfs)).
-:- system:use_module(library(wfs),[call_residual_program/2,call_delays/2,delays_residual_program/2,answer_residual/2]).
-%:- system:use_module(library(gui_tracer)). % autoloading swi_ide:guitracer/0 from /usr/lib/swipl/xpce/prolog/lib/gui_tracer
-%:- system:use_module(library(swi_compatibility)). %% autoloading swi_ide:auto_call/1 from /usr/lib/swipl/xpce/prolog/lib/swi_compatibility
-:- endif.
 
 
 %:- '$set_source_module'(baseKB).
@@ -229,67 +133,6 @@ load_plweb :-
   doc_enable(true),
   call(call,call(plweb:with_mutex(plweb_init, server_init)))),plweb).
 
-swish:is_really_module.
-
-run_lps_corner(File):- wdmsg(run_lps_corner(File)),!.
-run_lps_corner(File):- is_list(File),!,maplist(run_lps_corner,File).
-run_lps_corner(File):-
-   absolute_file_name(File,DB),
-   DB:use_module(library(lps_corner)), 
-   %listing(db:actions/1),
-   %listing(interpreter:actions/1),
-   interpreter:check_lps_program_module(DB),
-   DB:consult(DB),
-   interpreter:must_lps_program_module(DB),
-   %elsewhere:listing(DB:_),
-   DB:golps(X),
-   %listing(interpreter:lps_program_module/1),
-   dmsg(X).
-
-load_lps_corner:-!.
-load_lps_corner:- 
-  set_prolog_flag(cant_qsave_logicmoo,true),
-  run_lps_corner('/opt/logicmoo_workspace/packs_web/lps_corner/examples/binaryChop2.pl').
-     
-/*
-:- break.
-
-:- user:['/opt/logicmoo_workspace/packs_web/lps_corner/examples/goat.pl'].
-
-:- interpreter:must_lps_program_module(DB),
-   %elsewhere:listing(DB:_),   
-   DB:golps(X),
-   listing(lps_program_module/1),
-   format('~N~p.~n',[X]).
-
-:- break.
-
-:- party:['/opt/logicmoo_workspace/packs_web/lps_corner/examples/party.pl'].
-
-:- interpreter:must_lps_program_module(DB),
-   elsewhere:listing(DB:_),   
-   DB:golps(X),
-   listing(lps_program_module/1),
-   format('~N~p.~n',[X]).
-
-:- break.
-
-:- run_lps_corner('/opt/logicmoo_workspace/packs_web/lps_corner/examples/goat.pl').
-
-:- interpreter:must_lps_program_module(DB),
-   %elsewhere:listing(DB:_),   
-   DB:golps(X),
-   listing(lps_program_module/1),
-   format('~N~p.~n',[X]).
-
-:- break.
-
-blast_op(OP):- forall(current_op(_,XFX,OP),op(0,XFX,OP)).
-:- blast_op(false).
-:- blast_op(if).
-:- blast_op(then).
-:- blast_op(else).
-*/
 
 % ==============================================
 % ============= MUD SERVER CODE LOADING =============
@@ -309,8 +152,8 @@ do_setup_history:-
   add_history([
   (mpred_why(mudIsa(iCoffeeCup7, tSpatialThing))),
   (make:make_no_trace),
+  (update_changed_files),
   (shell('./PreStartMUD.sh')),
-  (run_lps_corner('lps_corner/examples/goat.pl')),
   ([pack(logicmoo_base/t/examples/fol/'einstein_simpler_03.pfc')]),
   ([pack(logicmoo_base/t/examples/fol/'einstein_simpler_04.pfc')]),
   (make:make_no_trace),
@@ -331,7 +174,6 @@ do_setup_history:-
   (input_to_forms("( #\\a #\\u0009 . #\\bell )",'$VAR'('O'),'$VAR'('Vs'))),
   (tstl),
   (qconsult_kb7166),
-  (load_lps_corner),
   (qsave_logicmoo),
   (start_all),
   (load_before_compile),
@@ -345,7 +187,6 @@ do_setup_history:-
    mud_baseKB,
    % rtrace,
    load_nomic_mu,% autoload_all([verbose(true)]), 
-   load_lps_corner,% autoload_all([verbose(true)]), 
    import_some,
    expose_all,
    %baseKB:start_runtime_mud,
@@ -365,7 +206,7 @@ zebra00:- [(pack(logicmoo_base/t/examples/fol/'einstein_simpler_04.pfc'))].
 :- set_prolog_flag(ec_loader,false).
 %:- abolish(system:trace,0).
 %:- asserta(system:trace).
-load_lpn :- prolog_load_context(directory,D), cd('/home/prologmud_server/lpn/www'),user:[server],cd(D).
+%load_lpn :- prolog_load_context(directory,D), cd('/home/prologmud_server/lpn/www'),user:[server],cd(D).
 try_zebra:- mpred_trace_all, zebra00, 
  forall(trait(P),listing(P)),
  add_history(clif_show),
@@ -410,7 +251,13 @@ expose_all:-
 
 :- multifile(rdf_rewrite:arity/2).
 :- dynamic(rdf_rewrite:arity/2).
+/*
+ (1) * /usr/local/share/swi-prolog/pack
+   (2)   /usr/share/swi-prolog/pack
+   (3)   /var/lib/snapd/desktop/swi-prolog/pack
+   (4)   /etc/xdg/swi-prolog/pack
 
+*/
 load_before_compile:- 
    mud_baseKB,
    /*
@@ -420,41 +267,45 @@ load_before_compile:-
    ignore(catch(pack_install(phil),_,true)),
    ignore(catch(pack_install(cplint_r),_,true)),
    */
-   cd('/tmp/tempDir'),
-   webui_load_swish_and_clio,
-   quietly(load_lps_corner).
+   ignore((
+    \+ exists_directory('/tmp/tempDir/') -> catch(shell('./PreStartMUD.sh'),_,true))),
+   ignore(( exists_directory('/tmp/tempDir') -> cd('/tmp/tempDir'))),
+   webui_load_swish_and_clio,   
+   add_history(start_network).
 
 %start_network:- 
 %   load_before_compile,!.
 
 start_network:- 
    load_before_compile,
-   egg_go,
-   ignore(catch(shell('./PreStartMUD.sh'),_,true)),
+   egg_go,   
    webui_start_swish_and_clio,
    threads,statistics,
    !.
    
 load_rest:-
+   nodebug,
    load_before_compile,
-   load_lps_corner,
    load_nomic_mu,
-   baseKB:use_module(library(logicmoo_clif)),
+   baseKB:ensure_loaded(library(logicmoo_ec)),
+   baseKB:ensure_loaded(library(logicmoo_cg)),
+   baseKB:ensure_loaded(library(narsese)),
+   baseKB:ensure_loaded(library(logicmoo_clif)),
    baseKB:ensure_loaded(library('logicmoo/common_logic/common_logic_sumo.pfc')),
-   add_history(try_zebra),
-   baseKB:ensure_loaded(library(logicmoo_mud)),
    baseKB:ensure_loaded(library(logicmoo_nlu)),
+   add_history(try_zebra),
    add_history(start_all),
    add_history(qsave_logicmoo),
-   nodebug,
+   system:reexport(pldata(kb_0988)),
    (current_prolog_flag(gui_tracer,true)->noguitracer;true),
    % run_before_qsave,
    do_setup_history,
+   nodebug,
+   baseKB:ensure_loaded(library(logicmoo_mud)),
    finish_processing_world,
   !.
 
-
-
+:- add_history(load_before_compile).
 
 qsave_logicmoo :-
    mud_baseKB,
@@ -488,7 +339,6 @@ start_rest:-
    mud_baseKB,
    % rtrace,
    %load_nomic_mu,% autoload_all([verbose(true)]), 
-   %load_lps_corner,% autoload_all([verbose(true)]), 
    import_some,
    expose_all,
    baseKB:start_runtime_mud,
@@ -499,6 +349,25 @@ start_rest:-
    baseKB:listing(mudAtLoc),
    threads,
    !.
+
+skip_sandboxing(F):- functor(P,F,1), 
+  (SF1 = (sandbox:F/1)),
+  sandbox:redefine_system_predicate(SF1),
+  sandbox:multifile(SF1),
+  sandbox:dynamic(SF1),
+  sandbox:asserta((P:-!)).
+
+skip_sandboxing:-
+ maplist(skip_sandboxing,
+  [safe_goal,
+   safe_call,
+   safe_directive,
+   safe_meta_predicate,
+   safe_source_directive,
+   safe_load_file_option,
+   safe_source_directive]).
+
+:- skip_sandboxing.
 
 baseKB:start_rest:- start_rest.
 
@@ -537,9 +406,5 @@ start_all :- start_network, start_rest.
 %:- (notrace(gtrace),nodebug).
 %:- guitracer.
 
-:- system:reexport(pldata(kb_0988)).
-
-:- use_module(library(logicmoo_cg)).
-:- use_module(library(narsese)).
 
 :- prolog. 
